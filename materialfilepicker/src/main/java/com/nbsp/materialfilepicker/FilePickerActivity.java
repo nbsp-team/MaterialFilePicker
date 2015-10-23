0b646b6c1f3be18641bb7c2479476369adbab889
Nickolay <lukaville@gmail.com>
2015-10-23 16:09:16 +0300

    activity adapter and fragment

---
+++ materialfilepicker/src/main/java/com/nbsp/materialfilepicker/FilePickerActivity.java
@@ -0,0 +1,15 @@
+package com.nbsp.materialfilepicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nickolay on 23.10.15.
+ */
+
+public class FilePickerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
